dcaa33fdec9f22a9f769fc2ea3437cf0fcee9589
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2022-09-16 15:54:56 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Hello, I will be using this for my web3 app</h1>
+    </div>
+  )
+}
+
+export default App
